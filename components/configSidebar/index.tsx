bad1672ef0b9240cc0feae3697d5723abc4cd78a
Manliot <mtejeda@smartsoftlabs.com>
2023-04-26 20:58:49 -0500

    feat(SIDEBAR):
    - Icons
    - Components
    - Context

---
+++ components/configSidebar/index.tsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import styles from './configSidebar.module.css'
+import { SidebarContext } from "@/context/sidebar/sideBarContext";
+import { Option } from "./ConfigSidebar.interface";
+
+
+export default function ConfigSidebar() {
+  const { activeOption, setActiveOption, OPTIONS } = useContext(SidebarContext)
+
+  const handleOptionClick = async (option: Option) => {
+    console.log(option)
+    setActiveOption(option);
+  };
+
+  return (
+    <aside className={styles.container}>
+      <div className={styles.titleContainer}>
+        <h2>Configuración</h2>
+      </div>
+      <div className={styles.optionsContainer}>
+        <ul
+          className={styles.listUl}
+        >
+          {OPTIONS.map((option) => {
+            return (
+              <li
+                key={option.option}
+                className={`${option === activeOption ? styles.active : ''}`}
+              >
+                {option.icon}
+                <button
+                  onClick={() => handleOptionClick(option)}
+                  className={`${styles.btn}`}
+                >{option.option}</button>
+              </li>
+            )
+          })
+          }
+        </ul>
+      </div>
+    </aside>
+  )
+}
