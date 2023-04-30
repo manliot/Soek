70cc34482f65fadf5705c5f5cd30245c14b6235b
Manliot <mtejeda@smartsoftlabs.com>
2023-04-29 20:09:46 -0500

    refactor(Hook user): change default user

--- customHooks/useUser.tsx
+++ customHooks/useUser.tsx
@@ -1,13 +1,18 @@
 import { useState, useEffect } from 'react'
 import { onAuthStateChanged } from '../services/firebase/googleAuth'
 import { User } from '../types/User.interface'
+
 export const USER_STATES = {
-  NOT_LOGGED: {},
-  NOT_KNOWN: undefined,
+  NOT_LOGGED: {
+    displayName: '',
+    email: '',
+    photoURL: '',
+    uid: '',
+  },
 }
 
 export function useUser(): User | undefined {
-  const [user, setUser] = useState(USER_STATES.NOT_KNOWN)
+  const [user, setUser] = useState(USER_STATES.NOT_LOGGED)
 
   useEffect(() => {
     onAuthStateChanged(setUser)
