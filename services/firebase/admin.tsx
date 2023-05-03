cefe54b7c9f8262865fee27da0054e1237760619
Manliot <mtejeda@smartsoftlabs.com>
2023-05-02 22:07:16 -0500

    refactor(FIREBASE): configure env variables for firebase admin

--- services/firebase/admin.tsx
+++ services/firebase/admin.tsx
@@ -1,13 +1,24 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_KEYS || "")
+const serviceAccount = {
+  "type": process.env.NEXT_PUBLIC_FIREBASE_KEYS_TYPE,
+  "project_id": process.env.NEXT_PUBLIC_FIREBASE_KEYS_PROJECT_ID,
+  "private_key_id": process.env.NEXT_PUBLIC_FIREBASE_KEYS_PRIVATE_KEY_ID,
+  "private_key": process.env.NEXT_PUBLIC_FIREBASE_KEYS_PRIVATE_KEY,
+  "client_email": process.env.NEXT_PUBLIC_FIREBASE_KEYS_PRIVATE_CLIENT_EMAIL,
+  "client_id": process.env.NEXT_PUBLIC_FIREBASE_KEYS_CLIENT_ID,
+  "auth_uri": process.env.NEXT_PUBLIC_FIREBASE_KEYS_AUTH_URI,
+  "token_uri": process.env.NEXT_PUBLIC_FIREBASE_KEYS_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.NEXT_PUBLIC_FIREBASE_KEYS_AUTH_PROVIDER_URL,
+  "client_x509_cert_url": process.env.NEXT_PUBLIC_FIREBASE_KEYS_CLIENT_CERT_URL
+}
 
 try {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
 } catch (error) {
-
+  console.error(error)
 }
 const firestore = admin.firestore()
 
