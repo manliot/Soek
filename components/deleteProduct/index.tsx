61bd45bcabdf17dff6520ac723a810d40fa048d1
Manliot <mtejeda@smartsoftlabs.com>
2023-05-03 23:50:12 -0500

    refactor(Config): hide product name input in delete page

--- components/deleteProduct/index.tsx
+++ components/deleteProduct/index.tsx
@@ -2,14 +2,14 @@ import { ProductForm } from "../productForm";
 import { ProductToAdd } from "../../types/Product.interface";
 
 export function DeleteProduct() {
-  const handleSubmit = (data: ProductToAdd ) => {
+  const handleSubmit = (data: ProductToAdd) => {
     console.log('delete', data);
   }
   return (
     <ProductForm
       action="delete"
       onSubmitAction={handleSubmit}
-      disabledInputs={['brand', 'price', 'aisle', 'url_img']}
+      disabledInputs={['name', 'brand', 'price', 'aisle', 'url_img']}
     />
   )
 }
