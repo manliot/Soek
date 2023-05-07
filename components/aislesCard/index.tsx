3a4f7b75ba8ee3735d1a71447105ce96f2043a5c
Manliot <mtejeda@smartsoftlabs.com>
2023-05-07 00:18:40 -0500

    feat(Aisles Card): Create component and styles of type minimal

---
+++ components/aislesCard/index.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image"
+import { AisleDB } from "@/types/Aisle.interface"
+import styles from './aislesCard.module.css'
+import { useAisleContext } from "@/context/aisles/aislesContext"
+import { useNavigation } from "@/hooks/useNavigation"
+
+export default function AislesCard(
+  { aisle,
+    type = 'all'
+  }: { aisle: AisleDB & { filtered: boolean }, type: 'minimal' | 'all' }) {
+
+  const { updateFilteredAisles } = useAisleContext()
+  const { navigateTo } = useNavigation()
+  const handleClick = () => {
+    updateFilteredAisles(aisle)
+    if (type === 'all')
+      navigateTo('/home')
+  }
+
+  return (
+    <div
+      className={
+        `${type === 'minimal' ? styles.containerMinimal : styles.container}
+        ${aisle.filtered ? styles.filtered : ''}`
+      }
+      onClick={handleClick}
+    >
+      {type === 'all' && (
+        <div>
+          <p>{aisle.aisleNumber}</p>
+        </div>
+      )}
+      <div
+        className={type === 'minimal' ? styles.aisleNameMinimal : styles.aisleName}
+      >
+        {aisle.name.split(":")[1]}
+      </div>
+      <Image
+        src={aisle.urlPhoto}
+        width={type === 'minimal' ? 40 : 50}
+        height={type === 'minimal' ? 40 : 50}
+        alt={`Aisle photo ${aisle.name.split(":")[1]}`}
+      />
+    </div>
+  )
+}
